725b49f15477bad053188e83750237f3a56bc6ca
Ullia <antales21@naver.com>
2021-11-16 17:07:53 +0900

    make video detail

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 import VideoList from './components/video_list/video_list';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
+  const seletVideo = (video) => {
+    setSelectedVideo(video);
+    console.log(video);
+  };
   const search = (query) => {
+    setSelectedVideo(null);
+    // loading... 이쯤에 구현
     youtube.search(query).then((item) => setVideos(item));
   };
 
@@ -17,7 +25,16 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={seletVideo} display={selectedVideo ? 'list' : 'grid'} />
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/video_detail/video_detail.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './video_detail.module.css';
+
+const VideoDetail = ({ video }) => {
+  return (
+    <section className={styles.detail}>
+      <iframe
+        className={styles.video}
+        type='text/html'
+        width='100%'
+        height='500'
+        src={`https://www.youtube.com/embed/${video.id}`}
+        frameborder='0'
+        allowfullscreen
+      ></iframe>
+      <div className={styles.videoInfo}>
+        <h3>{video.snippet.title}</h3>
+        <h4>{video.snippet.channelTitle}</h4>
+        <h5>{video.snippet.publishedAt}</h5>
+        <pre>{video.snippet.description}</pre>
+        <p>{video.snippet.tags}</p>
+
+        <div className={styles.sublist}></div>
+      </div>
+    </section>
+  );
+};
+
+export default VideoDetail;
